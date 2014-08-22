edb812c953c2de738197a97d3b420c6a057f12c4
Michael Bottjer <michaelbottjer@hotmail.com>
2014-08-22 14:53:30 +0100

    add connecting to Twitter API capability

---
+++ TwitterStream.py
@@ -0,0 +1,19 @@
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+
+
+
+class Listener(StreamListener):
+
+	def on_data(self, data):
+		print data
+		return True
+
+	def on_error(self, status):
+		print status
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+twitterStream = Stream(auth, Listener())
+twitterStream.filter(track=["netflix"])
